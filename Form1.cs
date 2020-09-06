e437d30502934bf31729fdf7a82ab6afbc609f24
Hong822 <kuestion822@gmail.com>
2020-09-06 23:15:17 +0900

    Add Driver, KPM site open

--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
-using System.IO;
-using OpenQA.Selenium;
-using OpenQA.Selenium.IE;
-using OpenQA.Selenium.Support.UI;
 
 namespace CS_KPMCreator
 {
@@ -15,18 +18,14 @@ namespace CS_KPMCreator
         private WebControl g_WebControl = new WebControl();
         private List<Dictionary<string, string>> g_TicketItemList = new List<Dictionary<string, string>>();
 
-        protected InternetExplorerDriverService _driverSerivce = null;
-        protected InternetExplorerOptions _options = null;
-        protected InternetExplorerDriver _driver = null;
+        protected String g_sWebtype = "";
+        protected DriverService _driverSerivce = null;
+        protected DriverOptions _options = null;
+        protected RemoteWebDriver _driver = null;
 
         public Form1()
         {
             InitializeComponent();
-
-            _driverSerivce = InternetExplorerDriverService.CreateDefaultService();
-            _driverSerivce.HideCommandPromptWindow = true;
-
-            _options = new InternetExplorerOptions();
         }
 
         private void bExcelSelect_Click(object sender, EventArgs e)
@@ -43,7 +42,7 @@ namespace CS_KPMCreator
 
                 //tExcelPath.Text = ExcelOpenDialog.FileName;
                 tExcelPath.Text = Dir + "\\KPM_Ticket_Creator_V1.xlsm";
-                
+
                 // Date read from Excel Files
                 ReadExcelValue();
             }
@@ -58,23 +57,48 @@ namespace CS_KPMCreator
             ap.Visible = true;
 
             // Fill in ticketItemList with ticket items
-            
             g_ExcelTool.FillDictionary(g_TicketItemList, ws_KPMCreate);
-
-            
         }
 
-        private void bStartCreation_Click(object sender, EventArgs e)
+        private void SetBrowser()
         {
-            // Open Browser
-            _driver = new InternetExplorerDriver(_driverSerivce, _options);
-
+            if (rbFirefox.Checked == true)
+            {
+                _driverSerivce = FirefoxDriverService.CreateDefaultService(); ;
+                _driverSerivce.HideCommandPromptWindow = true;
+                _options = new FirefoxOptions();                
+                _driver = new FirefoxDriver((FirefoxDriverService)_driverSerivce, (FirefoxOptions)_options);
+                g_sWebtype = "Firefox";
+            }
+            else
+            {
+                if (rbChrome.Checked == true)
+                {
+                    //_options = new ChromeOptions();
+                    //_driver = new ChromeDriver("C:\\KPM_Creator\\Driver\\chromedriver.exe", (ChromeOptions)_options);
+                    _driver = new ChromeDriver("C:\\KPM_Creator\\Driver\\chromedriver.exe");
+                    g_sWebtype = "Chrome";
+                }
+                else // Default = IE
+                {
+                    _driverSerivce = InternetExplorerDriverService.CreateDefaultService(); ;
+                    _driverSerivce.HideCommandPromptWindow = true;
+                    _options = new InternetExplorerOptions();                    
+                    _driver = new InternetExplorerDriver((InternetExplorerDriverService)_driverSerivce, (InternetExplorerOptions)_options);
+                    g_sWebtype = "IE";
+                }
+            }
+        }
+        private void GoToTheSite()
+        {
             rbB2B.Checked = true;
             rbB2C.Checked = false;
-
             if (rbB2C.Checked == true)
             {
-
+                string URL = "https://quasi.vw.vwg/kpm/kpmweb";
+                _driver.Navigate().GoToUrl(URL);
+                //_driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+                MessageBox.Show("Wait!!! Login and Go to Main page. And then press OK", "Please Login KPM");
             }
             else
             {
@@ -85,19 +109,38 @@ namespace CS_KPMCreator
 
                     if (sID != null && sPW != null)
                     {
-                        _driver.Navigate().GoToUrl("https://sso.volkswagen.de/kpmweb/Index.action");
-                        _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+                        //string URL = "https://" + sID + ":" + sPW + "@sso.volkswagen.de/kpmweb/Index.action";
+                        string URL = "https://www.naver.com";
+                        var wait = new WebDriverWait(_driver, new TimeSpan(0, 0, 30));
+
+                        try
+                        {
+                            _driver.Navigate().GoToUrl(URL);
+                        }
+                        catch (WebDriverException e)
+                        {
+                            Console.WriteLine(e);
+                        }
+                        
+                        MessageBox.Show("Wait!!! Login and Go to Main page. And then press OK", "Please Login KPM");
+                        int awer = 1;
                     }
                     else
                     {
                         _driver.Navigate().GoToUrl("https://" + sID + ":" + sPW + "@sso.volkswagen.de/kpmweb/Index.action");
                         _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-                    }                    
+                    }
                 }
             }
-            
-            // Start Ticket Creation 
-            g_WebControl.CreateTickets(g_TicketItemList);
+        }
+
+        private void bStartCreation_Click(object sender, EventArgs e)
+        {
+            SetBrowser();   // Get Browser
+
+            GoToTheSite();  // Go to KPM site
+                        
+            g_WebControl.CreateTickets(g_TicketItemList);   // Start Ticket Creation
         }
     }
 }
